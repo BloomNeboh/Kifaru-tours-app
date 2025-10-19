4c1e250bad53ea94fc3dac6648f73061718e0f84
BloomNE9Z <ellymchome21@gmail.com>
2025-10-19 10:59:31 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+console.log('Loaded MONGO_URI:', process.env.MONGO_URI);
 require('./config/envCheck');
 const express = require('express');
 const connectDB = require('./config/db');
